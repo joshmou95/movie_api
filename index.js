b92d7ee731cecc6024e8b6eaf5f994853978d9ed
joshmou95 <80426764+joshmou95@users.noreply.github.com>
2021-04-12 20:57:56 -0600

    Moved Json object to movies.js

--- index.js
+++ index.js
@@ -3,48 +3,7 @@ const express = require('express'),
 
 const app = express();
 
-let topMovies = { 'TopMovies' : [
-  {
-    title: 'Iron Man',
-    director: 'Jon Favreau'
-  },
-  {
-    title: 'The Incredible Hulk',
-    director: 'Louis Leterrier'
-  },
-  {
-    title: 'Iron Man 2',
-    director: 'Jon Favreau'
-  },
-  {
-    title: 'Thor',
-    director: 'Kenneth Branagh'
-  },
-  {
-    title: 'Captain America: The First Avenger',
-    director: 'Joe Johnston'
-  },
-  {
-    title: 'Marvel\'s The Avengers',
-    director: 'Joss Whedon'
-  },
-  {
-    title: 'Thor: The Dark World',
-    director: 'Alan Taylor'
-  },
-  {
-    title: 'Captain America: The Winter Soldier',
-    director: 'Anthony and Joe Russo'
-  },
-  {
-    title: 'Guardians of the Galaxy',
-    director: 'James Gunn'
-  },
-  {
-    title: 'Avengers: Age of Ultron',
-    director: 'Joss Whedon'
-  }
-]};
+let topMovies = require('./movies.js');
 
 app.use(express.static('public'));
 app.use(morgan('common'));
