2ce9f08cb1fe1b9264bd7a3fa94feb168f5b5df6
joshmou95 <80426764+joshmou95@users.noreply.github.com>
2021-04-12 20:57:37 -0600

    Created movies.js

---
+++ movies.js
@@ -0,0 +1,42 @@
+module.exports.topMovies = [
+  {
+    title: 'Iron Man',
+    director: 'Jon Favreau'
+  },
+  {
+    title: 'The Incredible Hulk',
+    director: 'Louis Leterrier'
+  },
+  {
+    title: 'Iron Man 2',
+    director: 'Jon Favreau'
+  },
+  {
+    title: 'Thor',
+    director: 'Kenneth Branagh'
+  },
+  {
+    title: 'Captain America: The First Avenger',
+    director: 'Joe Johnston'
+  },
+  {
+    title: 'Marvel\'s The Avengers',
+    director: 'Joss Whedon'
+  },
+  {
+    title: 'Thor: The Dark World',
+    director: 'Alan Taylor'
+  },
+  {
+    title: 'Captain America: The Winter Soldier',
+    director: 'Anthony and Joe Russo'
+  },
+  {
+    title: 'Guardians of the Galaxy',
+    director: 'James Gunn'
+  },
+  {
+    title: 'Avengers: Age of Ultron',
+    director: 'Joss Whedon'
+  }
+];
