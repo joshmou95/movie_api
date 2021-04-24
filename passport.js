7a23f050d5c3007dca9c37c0f84cbb75e239103b
joshmou95 <80426764+joshmou95@users.noreply.github.com>
2021-04-24 13:59:30 -0600

    updated passport

--- passport.js
+++ passport.js
@@ -39,7 +39,13 @@ passport.use(new LocalStrategy({
 // JWT is extracted from the header of the http request
 passport.use(new JWTStrategy({
   jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
-  secretOrKey: 'your_jwt_secret',
-}, (jwtPayload, callback) => Users.findById(jwtPayload._id)
-  .then((user) => callback(null, user))
-  .catch((error) => callback(error))));
+  secretOrKey: 'your_jwt_secret'
+}, (jwtPayload, callback) => {
+  return Users.findById(jwtPayload._id)
+    .then((user) => {
+      return callback(null, user);
+    })
+    .catch((error) => {
+      return callback(error)
+    });
+}));
