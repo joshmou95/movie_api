c45185318df6fd30e54c20dd66cd3b02a55dfeb1
joshmou95 <80426764+joshmou95@users.noreply.github.com>
2021-04-24 14:12:00 -0600

    updated Jwt auth strategy

--- passport.js
+++ passport.js
@@ -39,13 +39,7 @@ passport.use(new LocalStrategy({
 // JWT is extracted from the header of the http request
 passport.use(new JWTStrategy({
   jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
-  secretOrKey: 'your_jwt_secret'
-}, (jwtPayload, callback) => {
-  return Users.findById(jwtPayload._id)
-    .then((user) => {
-      return callback(null, user);
-    })
-    .catch((error) => {
-      return callback(error)
-    });
-}));
+  secretOrKey: 'your_jwt_secret',
+}, (jwtPayload, callback) => Users.findById(jwtPayload._id)
+  .then((user) => callback(null, user))
+  .catch((error) => callback(error))));
