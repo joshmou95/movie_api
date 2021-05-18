c0be400e903d7ae10aa223635ec2197c7b63ad2f
joshmou95 <80426764+joshmou95@users.noreply.github.com>
2021-05-17 20:02:58 -0600

    add domain to cors

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use(morgan('common'));
 app.use(express.json());
 
 app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', 'http://localhost:1234');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
