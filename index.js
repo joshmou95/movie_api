2a7f7be7e3f9b4dde9518d1a4bba2b958ab3bdd4
joshmou95 <80426764+joshmou95@users.noreply.github.com>
2021-05-18 20:15:29 -0600

    update cors

--- index.js
+++ index.js
@@ -35,12 +35,10 @@ const Users = Models.User;
 //   }
 // }));
 
-// app.use(cors());
-
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(cors({ credentials: true, origin: true }));
+app.use(cors());
 app.use(express.static('public'));
 app.use(morgan('common'));
 
