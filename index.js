1cbfb54fd90222e2db5c75050a81ed181623b214
joshmou95 <80426764+joshmou95@users.noreply.github.com>
2021-05-20 14:32:08 -0600

    updated cors allowedorigins

--- index.js
+++ index.js
@@ -22,10 +22,29 @@ require('./auth')(app);
 const Movies = Models.Movie;
 const Users = Models.User;
 
+const allowedOrigins = [
+  'https://myflixdb2000.herokuapp.com/', 'http://localhost:8080/', 'http://localhost:1234/'
+];
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        // If an origin is not on the list of allowed origins
+        const message =
+          'The CORS policy for this application does not allow access from origin ' +
+          origin;
+        return callback(new Error(message), false);
+      }
+      return callback(null, true);
+    }
+  })
+);
+
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(cors());
+
 app.use(express.static('public'));
 app.use(morgan('common'));
 
