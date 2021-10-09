eedf506a5248fe069e7ff6eb4829db41919d5db0
joshmou95 <80426764+joshmou95@users.noreply.github.com>
2021-10-09 17:25:31 -0600

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const mongoose = require('mongoose');
 const app = express();
 
 // app.use(cors());
-const allowedOrigins = ['http://localhost:8080', 'http://localhost:4200', 'https://joshmou95.github.io, https://myflixdb2000.herokuapp.com', 'http://localhost:1234', 'https://myflixmcu.netlify.app', 'https://myflixmcu.netlify.app/login'];
+const allowedOrigins = ['http://localhost:8080', 'http://localhost:4200', 'https://joshmou95.github.io', 'https://myflixdb2000.herokuapp.com', 'http://localhost:1234', 'https://myflixmcu.netlify.app', 'https://myflixmcu.netlify.app/login'];
 
 app.use(cors({
   origin: (origin, callback) => {
